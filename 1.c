f6aec03c8e7a6649afa6e8ce5db7ca59255fde84
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2020-05-28 21:13:34 +0900

    wakarimasen

--- 1.c
+++ 1.c
@@ -1,167 +1,172 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int i;
+#define NIL -1
 
-typedef struct cell
+typedef struct node
 {
     int key;
-    int prev;
-    int next;
-} cl;
+    struct node *parent;
+    struct node *left;
+    struct node *right;
+} nod;
 
-int allocate_object(cl *L, int *f);
-void list_insert(cl *L, int *head, int p, int new);
-void list_delete(cl *L, int *head, int a);
-void free_object(cl *L, int *f, int x);
-int list_minimum(cl *L, int *head);
+nod *tree_search(nod *T, int a);
+nod *tree_minimum(nod *T, nod *x);
+nod *tree_maximum(nod *T, nod *x);
+nod *tree_insert(nod *T, nod *x);
+nod *tree_delete(nod *T, nod *z);
+void inorder_tree_walk(nod *x);
+void tree_show(nod *T, nod *x);
 
 int main(void)
 {
-    cl List[50];     /* リストは cell の配列，数値数は 50 まで */
-    int n;           /* 数値の数は N */
-    int Data[50];    /* 数値を格納する配列は Data */
-    int head, freeL; /* リスト List の先頭，空きアドレスリストの先頭 */
-    FILE *fp;
+    nod *Tree;      /* ２分探索木を表す変数は Tree */
+    nod *x, *y, *z; /* 挿入，削除等に用いる node 型変数 */
+    int N;          /* 数値の数は N */
+    int Data[50];   /* 数値を格納する配列は Data */
+    int i;
+    FILE *fp;                    /* ファイル名は 128 文字まで対応可能にする */
     fp = fopen("data.txt", "r"); /* ファイルを読み込みモードで開く */
-    if (fp == NULL)
-        printf("ファイルの読み込みに失敗しました。\n");
-    else
-        printf("ファイルを読み込みます。\n");
-    fscanf(fp, "%d", &n); /* N をファイルから読み込む */
-    if (n > 50)
+    fscanf(fp, "%d", &N);        /* N をファイルから読み込む */
+    if (N > 50)
     {
         printf("N is too large, setting N = 50\n");
-        n = 50; /* N が 50 を超えるときは警告をした上で */
+        N = 50; /* N が 50 を超えるときは警告をした上で */
     }           /* N =50 に設定する */
-    for (i = 0; i < n; i++)
+    for (i = 0; i < N; i++)
     {
         fscanf(fp, "%d", &Data[i]); /* 整数値を順に読み込み，Data に格納する */
     }
     fclose(fp); /* 開いたファイルを閉じる */
-    printf("ファイルを閉じました。\n");
-
-    for (i = 0; i < 50; i++)
-    {
-        if (i == 0)
-        {
-            List[i].prev = -1;
-            List[i].next = 1;
-        }
-        else if (i == 49)
-        {
-            List[i].prev = 48;
-            List[i].next = -1;
-        }
-        else
-        {
-            List[i].prev = i - 1;
-            List[i].next = i + 1;
-        }
+    printf("ファイルを読み込みました。\n");
+    Tree = NULL; /* Tree の初期化，最初は空 */
+    /* Tree は２分探索木の根を指す*/
+    for (i = 0; i < N; i++)
+    { /* データ挿入部分のくりかえし */
+        x = (nod *)malloc(sizeof(nod));
+        x->key = Data[i]; /* 新しい頂点 x を生成して key などを指定 */
+        x->parent = x->left = x->right = NULL;
+        Tree = tree_insert(Tree, x); /* x を Tree に挿入して根の情報を更新*/
+        tree_show(Tree, Tree);
     }
-    printf("リストを初期化しました。\n");
-
-    head = -1;
-    freeL = 0; /* head freeL の初期化 */
-    for (i = 0; i < n; i++)
-    {
-        int emp = allocate_object(List, &freeL);
-        List[emp].key = Data[i];
-        list_insert(List, &head, -1, emp);
+    printf("データの格納が完了しました。\n");
+    for (i = 0; i < N; i++)
+    { /* データ削除部分のくりかえし */
+        z = tree_search(Tree, Data[i]);
+        tree_delete(Tree, z);
+        tree_show(Tree, Tree);
     }
-    printf("リストに数値を格納しました。\n");
-
-    int mini;
-    for (i = 0; i < n; i++)
-    {
-        mini = list_minimum(List, &head);
-        Data[i] = mini;
-        list_delete(List, &head, mini);
-        free_object(List, &freeL, mini);
-        printf("%d番目までソートしました。:%d\n", i + 1, Data[i]);
-    }
-    printf("ソートが完了しました。\n");
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", Data[i]);
-    }
-    printf("\n");
-
-    return 0;
 }
 
-int allocate_object(cl *L, int *f) //(L,freeL)
-{                                  /* リスト配列と空きアドレスリスト先頭はポインタ渡し */
-    int x;
-    if (*f == -1)
+nod *tree_search(nod *T, int a)
+{
+    nod *x = T;
+    while (x != NIL && x->key != a)
     {
-        printf("error: out of space\n");
-        x = -1;
-    } /* 空きアドレスがなければエラーメッセージを表示 */
-    else
-    {
-        x = *f;
-        *f = L[x].next;
+        if (a < x->key)
+            x = x->left;
+        else
+            x = x->right;
     }
+
     return x;
 }
 
-void list_insert(cl *L, int *head, int p, int new)
+nod *tree_minimum(nod *T, nod *x)
 {
-    L[new].prev = p;
-    L[new].next = *head;
-    if (*head != -1)
-        L[*head].prev = new;
-    *head = new;
-}
-
-void list_delete(cl *L, int *head, int a)
-{
-    int x = *head;
-    while (x != -1 && L[x].key != a)
+    while (x->left != NIL)
     {
-        x = L[x].next;
+        x = x->left;
     }
 
-    if (x != -1)
+    return (x);
+}
+
+nod *tree_maximum(nod *T, nod *x)
+{
+    while (x->right != NIL)
     {
-        int p = L[x].prev, q = L[x].next;
-        if (p != -1)
-        {
-            L[p].next = q;
-            if (q != -1)
-                L[q].prev = p;
-        }
+        x = x->right;
+    }
+
+    return (x);
+}
+
+nod *tree_insert(nod *T, nod *x)
+{
+    nod *r, *y, *z;
+    r = T; /* 根を変数 r に格納 */
+    y = NULL;
+    z = r;
+
+    while (z != NIL)
+    {
+        if (x->key < z->key)
+            y = z, z = z->left;
         else
-        {
-            *head = q;
-            if (q != -1)
-                L[q].prev = -1;
-        }
+            y = z, z = z->right;
     }
+
+    x->parent = y;
+
+    if (y == NIL)
+        r = x;
+    else if (y->key < x->key)
+        y->right = x->key;
+    else
+        y->left = x->left;
+
+    return (r);
 }
 
-void free_object(cl *L, int *f, int x)
+nod *tree_delete(nod *T, nod *z)
 {
-    L[x].next = *f;
-    *f = x;
-}
+    nod *x, *y, *p;
 
-int list_minimum(cl *L, int *head)
-{
-    int x, y;
-    x = *head;
-    y = x;
-    int min = INT_MAX;
-    while (x != -1)
+    if (z->left == NULL || z->right == NULL)
+        y = z;
+    else
+        y = tree_minimum(T, z->right);
+
+    p = y->parent;
+
+    if (y->right != NULL || y->left != NULL)
+        x = (y->left != NULL ? y->left : y->right);
+    else
+        x = NIL;
+
+    if (p == NIL)
+        T = x;
+    else
     {
-        if (L[x].key < min)
-        {
-            min = L[x].key;
-            y = x;
-        }
-        x = L[x].next;
+        if (p->key < y->key)
+            p->right = x;
+        else
+            p->left = x;
     }
-    return y;
+
+    if (y != z)
+        z->key = y->key;
+
+    free(y);
+
+    return (T);
+}
+
+void inorder_tree_walk(nod *x)
+{
+    if (x != NULL)
+    {
+        inorder_tree_walk(x->left);
+        printf(" %d", x->key);
+        inorder_tree_walk(x->right);
+    }
+}
+
+void tree_show(nod *T, nod *x)
+{
+    inorder_tree_walk(x);
+    printf("\n");
+    printf("MAX:%d, MIN:%d\n", *tree_maximum(T, x), *tree_minimum(T, x));
 }
